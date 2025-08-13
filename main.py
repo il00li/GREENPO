cfedf1c2ab8e0dfcca9176e2e9a233b22656f0a3
il00li <yl99383899@gmail.com>
2025-08-13 06:57:47 +0300

    Update and rename deepseek_python_20250813_c5bcb2.py to main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from urllib.parse import quote
 app = Flask(__name__)
 
 # تكوين البوت
-TOKEN = "8110119856:AAEMbomUhyXXrR8Y-YvmTJR4jmDP1-y-tQo"
+TOKEN = "8110119856:AAHd8yHv54d50GzdvSeRz3h0XvixPu4rg-I"
 ADMIN_ID = 7251748706
 CHANNELS = ["@crazys7", "@AWU87"]
 LUMMI_API_KEY = "lummi-b06d12ba02329efb74404de07e20b434aff295de34419f35c56eb3e200f05a71"
